72a1af17f1fb674ce6452b67c07d73c8d832d1de
Sebastian Schmider <91938277+Sebi667@users.noreply.github.com>
2022-01-28 23:26:29 +0100

    new

--- A8_Formulare + Webapp/JS/Server.js
+++ A8_Formulare + Webapp/JS/Server.js
@@ -1,52 +1,126 @@
 "use strict";
-//namespace Aufgabe8{} <-- nor in namespace to have one node_modulesFolder
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
 const mongo = require("mongodb");
 const hostname = "127.0.0.1"; // localhost
 const port = 3500;
+const pfad = "/concertEvents";
+const pfadDelet = "/delet";
 const mongoUrl = "mongodb://localhost:27017"; // locale MongoDB
-let mongoClient = new mongo.MongoClient(mongoUrl); //mognoClinent
+const dbCollection = "eventNode";
+const db = "Events";
+let mongoClient = new mongo.MongoClient(mongoUrl); //mongo Client 
 const server = http.createServer(async (request, response) => {
     response.statusCode = 200;
     response.setHeader("Access-Control-Allow-Origin", "*");
     let url = new URL(request.url || "", `http://${request.headers.host}`);
+    console.log("\x1b[33m", "the rquest path: " + url.pathname);
     switch (url.pathname) {
-        case "/todo": {
-            await mongoClient.connect();
+        case pfad: {
+            try {
+                console.log("\x1b[33m", "conecting to DB...");
+                const client = await mongoClient.connect();
+                if (!client) {
+                    console.log("\x1b[33m", "fetshing the Database...");
+                    mongoClient.db(db);
+                }
+            }
+            catch (error) {
+                console.error("\x1b[31m", "connection time out wiht DB");
+                console.log("\x1b[0m");
+                response.statusCode = 404;
+                return;
+            }
+            finally {
+                mongoClient.close();
+            }
+            console.log("\x1b[32m", "this is the request: " + request.method);
             switch (request.method) {
                 case "GET":
-                    console.log("TODO-GET");
+                    try {
+                        console.log("\x1b[33m", "fetshing the DatabaseCollection...");
+                        await mongoClient.connect();
+                        let text = await dbGet();
+                        response.setHeader("Content-Type", "application/json");
+                        response.end(text);
+                        console.log("\x1b[33m", "sending to client: " + text);
+                    }
+                    catch (error) {
+                        console.error("\x1b[31m", error);
+                    }
+                    finally {
+                        mongoClient.close();
+                    }
                     break;
                 case "POST":
-                    console.log("TODO-POST");
+                    let input;
+                    request.on("data", (data) => {
+                        input += data;
+                    });
+                    try {
+                        request.on("end", async () => {
+                            input = input.replace("undefined", "");
+                            console.log("\x1b[33m", "Data: " + input);
+                            console.log("\x1b[33m", "sending Data...");
+                            await mongoClient.connect();
+                            await dbSet(input);
+                        });
+                    }
+                    catch (error) {
+                        console.error("\x1b[31m", error);
+                    }
+                    finally {
+                        mongoClient.close();
+                    }
                     break;
             }
+            break;
         }
-        case "/todoS": {
-            await mongoClient.connect();
-            switch (request.method) {
-                case "GET":
-                    console.log("TodoS-GET");
-                    break;
+        case pfadDelet:
+            let eventID = url.searchParams.get("EventID");
+            console.log("\x1b[33m", "request to delet an elment ID: " + eventID);
+            try {
+                await mongoClient.connect();
+                await dbRemove(eventID);
             }
-        }
+            catch (error) {
+                console.error("\x1b[31m", error);
+            }
+            finally {
+                mongoClient.close();
+            }
+            break;
         default:
             response.statusCode = 404;
     }
 });
-//search for DB-Content
-async function dbFind(db, collection, requestObject, response) {
+async function dbGet() {
     let result = await mongoClient
         .db(db)
-        .collection(collection)
-        .find(requestObject)
+        .collection(dbCollection)
+        .find()
         .toArray();
-    // console.log(result, requestObject); // bei Fehlern zum Testen
-    response.setHeader("Content-Type", "application/json");
-    response.write(JSON.stringify(result));
+    console.log("\x1b[32m", "got the data");
+    console.log("\x1b[32m", result);
+    return JSON.stringify(result);
+}
+async function dbSet(event) {
+    console.log("\x1b[33m", "send Data:" + JSON.parse(event) + +" " + (JSON.parse(event).id));
+    await mongoClient.db(db).collection(dbCollection).insertOne(JSON.parse(event));
+    console.log("\x1b[32m", "Data recived");
+}
+async function dbRemove(eventID) {
+    console.log("\x1b[33m", "removing elment with ID: " + eventID);
+    try {
+        await mongoClient.db(db).collection(dbCollection).deleteOne({ _id: new mongo.ObjectId(eventID) }); //new mongo.ObjectId(eventID)
+        console.log("\x1b[32m", "Data removed");
+    }
+    catch (error) {
+        console.log("\x1b[32m", error);
+    }
 }
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.clear();
+    console.log("\x1b[32m", `Server running at http://${hostname}:${port}/`);
 });
 //# sourceMappingURL=Server.js.map
---
+++ A8_Formulare + Webapp/README.md
@@ -0,0 +1,49 @@
+# Grundlagen Interaktiver Systeme
+## Info
+* Name: Sebastian Schmider
+* Matrikel-Nummer: 268040
+* Kurs: MIB 2 Gruppe A
+
+## Aufgabe 8 - Formulare + Webapp
+* Link zum Source-Code: https://github.com/Sebi667/HTML_Grundlagen/tree/main/A8_Formulare%20%2B%20Webapp
+* MainPage: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A8_Formulare%20%2B%20Webapp/MainPage.html 
+
+## Aufgabe 6 - Server und Requests
+Hinweis: Server muss per Konsolencommand (node NodeJS\NodeServer.js) gestartet werden, um den vollen Funktionsrahmen zu gewährleisten.
+* Link zum Source-Code:
+* MainPage: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A6_Server%20und%20Requests/MainPage.html 
+
+## Aufgabe 4/5 - TS DOM und localStorage
+* Link zum Source-Code:
+* Events.html: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A4_TS%20DOM_und_localStorage/Events.html
+* style.css: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A4_TS%20DOM_und_localStorage/style.css
+* script.ts: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A4_TS%20DOM_und_localStorage/script.ts
+* script.js: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A4_TS%20DOM_und_localStorage/script.js
+
+## Aufgabe 3 - TypeScript
+* Link zum Source-Code:
+* script_vorlage1.ts: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A3_TypeScript/script_vorlage1.ts
+* index.html: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A3_TypeScript/index.html
+
+## Aufgabe 2 - CSS
+* Link zum Source-Code:
+* A2_Frontpage: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A2_CSS/A2_Frontpage.html
+* A2_Events: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A2_CSS/A2_Events.html
+* style.css: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A2_CSS/style.css
+## Update:
+* Google-Fonts richtig eingebunden
+* Überschriften werden jetzt per Animation "geschrieben"
+* Bei Hover-Effekten wird jetzt eine betimmte transition-duration verwendet.
+* Im zuge dieser Änderungen wurden pseudoelemente verwendet.
+* Responsive-Design wurde überarbeitet - es sollten jetzt beim Skalieren des Fensters keine Bildelemente den Rand verlassen
+
+## Aufgabe 1 - HTML Grundlagen
+* Link zum Source-Code:
+* A1_Frontpage: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A1_HTML/A1_Frontpage.html
+* A1_Events: https://github.com/Sebi667/HTML_Grundlagen/blob/main/A1_HTML/A1_Events.html 
+* Link zur Pages-Seite: https://github.com/Sebi667/HTML_Grundlagen
+## Update:
+* Auf der Frontpage wurde eine Sprungmarke zum eingebetteten Video ergänzt.
+* Schreibfehler im charset "uf-8" wurde auf "utf-8" korrigiert.
+* Alle Änderungen wurden zudem auch im Github aktualisiert.
+
